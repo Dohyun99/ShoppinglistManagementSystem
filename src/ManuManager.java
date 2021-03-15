aa4c7929ece2ab888582c21f51effae3e61ea4dd
Dohyun99 <hgh990@naver.com>
2021-03-16 01:48:37 +0900

    add ManuManager

---
+++ src/ManuManager.java
@@ -0,0 +1,14 @@
+
+public class ManuManager {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("1. Add shopping list");
+		System.out.println("2. View shopping list");
+		System.out.println("3. Edit shopping list");
+		System.out.println("4. Delete all");
+		System.out.println("5. Exit");
+		
+	}
+	
+}
